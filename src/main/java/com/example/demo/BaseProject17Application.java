4cc0f3e894581fe73c68ed2586efee3f5221d40f
GBTC440005ur <EEnamor1@mongtomerycollege.edu>
2018-03-13 16:51:03 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/BaseProject17Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseProject17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseProject17Application.class, args);
+	}
+}
