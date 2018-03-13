046c571b819f60deebec7575b75fbc0bec081d9f
GBTC440005ur <EEnamor1@mongtomerycollege.edu>
2018-03-13 16:55:56 -0400

    Adding bootstrap files to code

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -11,7 +11,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
     @Override
     public void configure(HttpSecurity http) throws Exception
     {
-        http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
+        http.authorizeRequests().antMatchers("/","/css/**","/js/**").permitAll().anyRequest().authenticated().and().formLogin().and().httpBasic();
     }
 
     @Override
